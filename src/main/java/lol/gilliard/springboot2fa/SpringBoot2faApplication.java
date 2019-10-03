59462b918a817ba62d27fbd22fd8707ec8240e9b
Matthew Gilliard <matthew@gilliard.lol>
2019-10-03 14:25:03 +0100

    Initial project setup as downloaded from Initializr

---
+++ src/main/java/lol/gilliard/springboot2fa/SpringBoot2faApplication.java
@@ -0,0 +1,13 @@
+package lol.gilliard.springboot2fa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2faApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2faApplication.class, args);
+	}
+
+}
