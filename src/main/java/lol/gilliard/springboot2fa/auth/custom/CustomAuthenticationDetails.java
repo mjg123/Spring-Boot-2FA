da0a4ec01c7e6ade88326b4d43702902756b0092
Matthew Gilliard <matthew@gilliard.lol>
2020-10-20 11:12:17 +0100

    Login phase is DONE
    Users will now need to use the 2FA code from their app when they log
    in.

--- src/main/java/lol/gilliard/springboot2fa/auth/custom/CustomAuthenticationDetails.java
+++ src/main/java/lol/gilliard/springboot2fa/auth/custom/CustomAuthenticationDetails.java
@@ -6,13 +6,19 @@ import javax.servlet.http.HttpServletRequest;
 
 public class CustomAuthenticationDetails extends WebAuthenticationDetails {
 
+    private String user2FaCode;
+
     public CustomAuthenticationDetails(HttpServletRequest request) {
 
-        // TODO: (login) This would be a good place to pull any extra parameters that we might
+        // TODO: (DONE login) This would be a good place to pull any extra parameters that we might
         // need to authenticate our user out of the HttpServletRequest.
         // We could store them in fields here and access them in an AuthenticationProvider
-
         super(request);
+        System.out.println(request.getRequestURI() + " :: " + request.getParameter("2facode"));
+        this.user2FaCode = request.getParameter("2facode");
     }
 
+    public String getUser2FaCode() {
+        return user2FaCode;
+    }
 }
--- src/main/java/lol/gilliard/springboot2fa/auth/custom/CustomAuthenticationProvider.java
+++ src/main/java/lol/gilliard/springboot2fa/auth/custom/CustomAuthenticationProvider.java
@@ -1,7 +1,12 @@
 package lol.gilliard.springboot2fa.auth.custom;
 
+import com.amdelamar.jotp.OTP;
+import com.amdelamar.jotp.type.Type;
+import lol.gilliard.springboot2fa.UserDto;
 import lol.gilliard.springboot2fa.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -9,11 +14,15 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 public class CustomAuthenticationProvider extends DaoAuthenticationProvider {
 
-// TODO: (login) This might be useful if we want to look up a UserDto by name
-//    @Autowired
-//    private UserRepository userRepository;
+// TODO: (DONE login) This might be useful if we want to look up a UserDto by name
+    @Autowired
+    private UserRepository userRepository;
 
     public CustomAuthenticationProvider(UserDetailsService userDetailsService,
                                         PasswordEncoder passwordEncoder) {
@@ -25,11 +34,33 @@ public class CustomAuthenticationProvider extends DaoAuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 
-        // TODO: (login) we can provide extra authentication checks here.
+        // TODO: (DONE login) we can provide extra authentication checks here.
 
         // `authentication.getName()` gives use the username that is trying to log in
         // `(CustomAuthenticationDetails) authentication.getDetails()` gives us an object
         //     which has extra info that the user provided when they tried to log in.
+
+        UserDto userDto = userRepository.findByUsername(authentication.getName());
+
+        if (userDto != null){
+
+            try {
+                String serverGeneratedCode = OTP.create(userDto.getSecret(), OTP.timeInHex(), 6, Type.TOTP);
+
+                CustomAuthenticationDetails userProvidedLoginDetails = (CustomAuthenticationDetails) authentication.getDetails();
+
+                logger.info("Server code " + serverGeneratedCode);
+                logger.info("User code " + userProvidedLoginDetails.getUser2FaCode());
+
+                if (!serverGeneratedCode.equals(userProvidedLoginDetails.getUser2FaCode())){
+                    throw new BadCredentialsException("User's 2FA code didn't match server code");
+                }
+
+            } catch (Exception e) {
+                logger.error("Oh no", e);
+                throw new AuthenticationServiceException("Failed to generate server-side 2FA code");
+            }
+        }
 
         // throw an AuthenticationException, or a subclass like BadCredentialsException here
         // to reject this login attempt.
